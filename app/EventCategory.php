c48ad7180a75501215a92d4dbf8bb559082d0aa1
Ashish Patel <ashishpatel0720@gmail.com>
2017-10-07 00:22:48 +0530

    BUGFIX: extra tabs and newlines removed from category descriptions

--- app/EventCategory.php
+++ app/EventCategory.php
@@ -15,16 +15,13 @@ class EventCategory extends Model
 {
     public static $categories=[
         ['name'=>'pronites',
-            'description'=>'The time is almost upon us. The stage is almost set, its now upto us to make use of it. 
-             Come forward and enjoy your favourite nights with us. fun awaits as promised.',
+            'description'=>'The time is almost upon us. The stage is almost set, its now upto us to make use of it. So, Come forward and enjoy your favourite nights with us,fun awaits as promised.',
             'imageurl'=>'imgUrl'],
 
         ['name'=>'competitions',
             'description'=>
-        'Technology refers to the practical application of knowledge and also includes the capability and skills required to apply the knowledge.
-         Therefore,Techfest presents a series of competitions intended to deepen the understanding of advance solutions in the
-         areas of Science and Technology. 
-         We invite you all to come forward think hard for  extremely mind bending competitions of this year'
+        'Technology refers to the practical application of knowledge and also includes the capability and skills required to apply the knowledge.Therefore,Techfest presents a series of competitions intended to deepen the understanding of advance solutions in the areas of Science and Technology. 
+         We invite you all to come forward think hard for  extremely mind bending competitions of this year.'
             ,'imageurl'=>'imgUrl'],
 
 //        ['name'=>'informals','description'=>'','imageurl'=>'imgUrl'],
